180d05462fd620d45c5861680a333c35409df75e
herlangga666 <aherlangga01@gmail.com>
2019-08-21 15:15:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragmenttugas/NotifikasiFragment.java
@@ -0,0 +1,18 @@
+package com.example.fragmenttugas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NotifikasiFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.notifikasi_fragment , container , false);
+    }
+}
